57bbea2167f46bc533d09a9f58344d2ac1b6c9dc
Ash-Liu425 <1159977302@qq.com>
2026-05-01 20:10:27 +0800

    v1.3

--- cpp-code/hello.cpp
+++ cpp-code/hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+//主要功能：这是一个简单的C++程序，演示了如何使用输入输出流、字符串和循环结构。程序输出一个问候语，并展示了一个简单的循环。
 int main() {
     std::string name = "World";
     std::cout << "Hello, " << name << "!" << std::endl;
